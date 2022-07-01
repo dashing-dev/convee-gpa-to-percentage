5bf353ee5f902901a6f347580ac333f4d1ab1772
Sanjay Bhandari <93391951+dashing-dev@users.noreply.github.com>
2022-07-01 22:31:13 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function fn1()
+{
+    var num = document.getElementById("gpa").value;
+    percentage = num*100/4;
+    document.getElementById("test").innerHTML = 'Result : '+percentage ;
+    
+}
+
+
